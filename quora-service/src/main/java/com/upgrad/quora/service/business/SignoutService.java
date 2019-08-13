3b160bc787c12fa74a749f2c92b8ec3a346b5273
Siva <sivansbhfe@gmail.com>
2019-08-13 23:01:37 +0530

    All relevant exceptions handled for signup, signin and signout

--- quora-service/src/main/java/com/upgrad/quora/service/business/SignoutService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignoutService.java
@@ -15,12 +15,23 @@ public class SignoutService {
 
     @Transactional(propagation = Propagation.REQUIRED)
     public String signOut (final String authorization) throws SignOutRestrictedException {
-        return userDao.signOut(authorization);
+
+        //Check if the user has an access token and it is valid
+        if(userDao.hasUserSignedIn(authorization) && userDao.isUserAccessTokenValid(authorization)){
+            return userDao.signOut(authorization);
+        } else {
+            throw new SignOutRestrictedException("SGR-001","User is not Signed in");
+        }
     }
 
+    //To check if the user with the access token is signed in / access token exists in the table
+    //Returns boolean based on whether the access token is present in the table
     public boolean hasUserSignedIn (final String authorization)  {
         return userDao.hasUserSignedIn(authorization);
     }
+
+    //To check if the user has a valid acces token / access token exists and is valid
+    //Returns boolean based on 2 factors: The expires_at time is greater than current time and LogoutAt is null
     public boolean isUserAccessTokenValid (final String authorization)  {
         return userDao.isUserAccessTokenValid(authorization);
     }
