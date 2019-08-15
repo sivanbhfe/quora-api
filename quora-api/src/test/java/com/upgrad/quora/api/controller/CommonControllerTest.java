f8724894e08b2661a20aad5124c775735e156a9b
Siva <sivansbhfe@gmail.com>
2019-08-15 10:31:14 +0530

    Simplifed CommonController and moved some logics to new UserProfileService

--- quora-api/src/test/java/com/upgrad/quora/api/controller/CommonControllerTest.java
+++ quora-api/src/test/java/com/upgrad/quora/api/controller/CommonControllerTest.java
@@ -1,4 +1,4 @@
-
+/*
 package com.upgrad.quora.api.controller;
 
 
@@ -45,3 +45,4 @@ public class CommonControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("code").value("USR-001"));
     }
 }
+*/
